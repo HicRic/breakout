5d7ca85f6ab736436eb34d1278c3b08466f5a247
Ricky Willis <ricky.willis@spaceapegames.com>
2019-12-22 19:30:00 +0000

    setup some physics config and a LimitDOFJoint from sample code to constrain ball to XY axis only

---
+++ breakout/Assets/Scripts/Config/Config.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Config/Root Config")]
+public class Config : ScriptableObject
+{
+    public PaddleCfg Paddle;
+
+    private static Config instance;
+    public static Config Instance => instance ?? (instance = Resources.Load<Config>("Config"));
+}
---
+++ breakout/Assets/Scripts/Config/PaddleCfg.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PaddleCfg
+{
+    public GameObject Prefab;
+    public float MoveSpeed;
+}
+
