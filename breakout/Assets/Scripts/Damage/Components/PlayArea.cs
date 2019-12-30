73213b57d6da523bea0b2c50d2b1f91acb86ea27
Ricky Willis <ricky.willis@spaceapegames.com>
2019-12-30 15:15:45 +0000

    Added PlayArea system to destroy tagged entities if they are not contained within any play area.

---
+++ breakout/Assets/Scripts/Damage/Components/PlayArea.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct PlayArea : IComponentData
+{
+    public AABB Value;
+}
+
---
+++ breakout/Assets/Scripts/Damage/PlayAreaSystem.cs
@@ -0,0 +1,52 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class PlayAreaSystem : JobComponentSystem
+{
+    private EntityCommandBufferSystem ecbSystem;
+
+    protected override void OnCreate()
+    {
+        ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var playAreas = GetEntityQuery(typeof(PlayArea)).ToComponentDataArray<PlayArea>(Allocator.TempJob);
+
+        BoundsJob job = new BoundsJob
+        {
+            PlayAreas = playAreas,
+            ECB = ecbSystem.CreateCommandBuffer().ToConcurrent()
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+
+    [RequireComponentTag(typeof(DestroyOutsidePlayAreaTag))]
+    private struct BoundsJob : IJobForEachWithEntity<Translation>
+    {
+        [DeallocateOnJobCompletion]
+        public NativeArray<PlayArea> PlayAreas;
+
+        public EntityCommandBuffer.Concurrent ECB;
+
+        public void Execute(Entity entity, int index, [ReadOnly] ref Translation translation)
+        {
+            foreach (PlayArea playArea in PlayAreas)
+            {
+                // (Contains() is pure)
+                // ReSharper disable once PossiblyImpureMethodCallOnReadonlyVariable
+                if (playArea.Value.Contains(translation.Value))
+                {
+                    return;
+                }
+            }
+
+            ECB.DestroyEntity(index, entity);
+        }
+    }
+}
+
